9f7aa81fbeaac938b82268030757010ea2e041f7
andy910829 <71509005+andy910829@users.noreply.github.com>
2023-03-07 16:27:24 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -12,10 +12,18 @@ class tool:
             "account": "andy625018171@gmail.com",
             "password": "9ceae5f376d2713c47b7b1d520fc3062e8eeb5689dcbffe424e7d697d0d4f8df",
             "token": "",
-            "type": "admin"
+            "type": "admin",
+            "name":"潘孟鉉"
         }
-        self.collection.insert_one(admin)
+        self.collection.update_one({"account":admin["account"]},{"$set":admin})
         print("done!")
+    
+    def update_pro(self):
+        pro_info = self.collection.find_one({"account":"mspan@ntut.edu.tw"})
+        pro_info["type"] = "admin"
+        self.collection.update_one({"account":"mspan@ntut.edu.tw"},{"$set":pro_info})
+        print("change pro info done!!")
 
 if __name__ == "__main__":
     tool().insert_admin()
+    tool().update_pro()
