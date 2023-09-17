16e983de4ece1928364d42ca0daa06738d311bc6
andy910829 <71509005+andy910829@users.noreply.github.com>
2023-09-18 00:21:55 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -13,38 +13,45 @@ class tool:
         for group in groups:
             group_leader_id = group["leader"]["student_id"]
             group_leader_name = group["leader"]["name"]
-            self.check_name(group_leader_name, group_leader_id,group["group_id"])
+            self.check_name(group_leader_name,
+                            group_leader_id, group["group_id"])
             for member in group["member"]:
                 member_id = member["student_id"]
                 member_name = member["name"]
                 self.check_name(member_name, member_id, group["group_id"])
 
-    def check_name(self ,user_name, user_id, group_id):
+    def check_name(self, user_name, user_id, group_id):
         user = self.user_collection.find_one({"student_id": user_id})
         if user["name"] != user_name:
             print(user_id, user_name)
-            change = input(f"Do you want to change {user_name} to {user['name']}? (y/n)")
+            change = input(
+                f"Do you want to change {user_name} to {user['name']}? (y/n)")
             if change == "y":
                 if user['user_identity'] == 'group_leader':
-                    self.group_collection.update_one({"group_id": user_id}, {"$set": {"group_leader.name": user['name']}})
+                    self.group_collection.update_one(
+                        {"group_id": user_id}, {"$set": {"group_leader.name": user['name']}})
 
     def check_judge(self):
         for group in self.group_collection.find():
             for group_judge in group['judge']:
-                judge = self.user_collection.find_one({"name":group_judge['name']})
+                judge = self.user_collection.find_one(
+                    {"name": group_judge['name']})
                 if group['group_id'] not in judge['groups']:
                     print(group['group_id'], judge['name'])
 
     def check_group(self):
-        for user in self.group_collection.find({"acedemic_year":"112"}):
+        for user in self.user_collection.find({"acedemic_year": "112"}):
             cnt = 0
             group = self.group_collection.find_one({"group_id":user['group_id']})
-            for member in group['member']:
-                if member['student_id'] == user['student_id']:
-                    cnt+=1
-                    break
-            if cnt>1:
-                print(user['group_id'], user['student_id'])
+            try:
+                for member in group['member']:
+                    if member['student_id'] == user['student_id']:
+                        cnt += 1
+                if cnt > 1:
+                    print(user['group_id'], user['student_id'])
+            except:
+                print(user['group_id'])
+                print(group)
 
 
 if __name__ == '__main__':
